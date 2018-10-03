4103598ffbf35ff16b4a9b662cf695d5fc91d33f
Antonyraj, Florence <Florence.antonyraj@fmr.com>
2018-10-03 11:28:13 -0400

    Inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 Insert into MARKET_SECTOR Values(null,'Financial','Financial, Northeast Region');
 Insert into MARKET_SECTOR Values(null,'Marketing','Marketing, Southwest Region');
+Insert into SECURITIES Values('AAPL','Apple Inc', 'American multinational technology company', 1);
+Insert into SECURITIES Values('GOOGL','Google LLC', 'Internet-related services and products', 1);
+
