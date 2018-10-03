cb136d3401c7d750f1eeaa1ba31b70538193295d
Rambabu Bandam <rambabu.bandam@fmr.com>
2018-10-03 10:28:12 -0400

    adding domain info

---
+++ src/main/java/com/acme/acmetrade/domain/Security.java
@@ -0,0 +1,44 @@
+package com.acme.acmetrade.domain;
+
+public class Security {
+    private String symbol;
+    private String companyName;
+    private int sectorId;
+    private String description;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getSectorId() {
+        return sectorId;
+    }
+
+    public void setSectorId(int sectorId) {
+        this.sectorId = sectorId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+
+}
---
+++ src/main/java/com/acme/acmetrade/endpoints/SecurityEndpoint.java
@@ -0,0 +1,28 @@
+package com.acme.acmetrade.endpoints;
+
+
+import com.acme.acmetrade.domain.Security;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class SecurityEndpoint {
+	
+	@RequestMapping(path = "/securities" , method = RequestMethod.GET)
+	public List<Security> listSecurities() {
+		return new ArrayList<Security>();
+	}
+
+	@RequestMapping(path = "/securities/{symbol}" , method = RequestMethod.GET)
+	public Security listSecuritY(@PathParam("symbol") String symbol) {
+		return new Security();
+	}
+
+	@RequestMapping(path = "/securities" , method = RequestMethod.POST)
+	public Security addSecurity(@RequestBody Security security) {
+		return security;
+	}
+}
