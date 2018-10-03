9c9a36f7c1108c68bc916c05f7dc0f7394b2f500
William Ovanna <william.ovanna@fmr.com>
2018-10-03 13:01:08 -0400

    Implemented SecurityRepository methods, bug fix

--- src/main/java/com/acme/acmetrade/repository/SecurityRepository.java
+++ src/main/java/com/acme/acmetrade/repository/SecurityRepository.java
@@ -1,11 +1,14 @@
 package com.acme.acmetrade.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
@@ -15,40 +18,50 @@ import com.acme.acmetrade.domain.Security;
 @Repository
 public class SecurityRepository {
 
+	private final JdbcTemplate jdbcTemplate;
 
-    private final JdbcTemplate jdbcTemplate;
+	@Autowired
+	public SecurityRepository(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
-    @Autowired
-    public SecurityRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-   
-    public int saveSecurity(Security security){
-        return jdbcTemplate.update("INSERT INTO SECURITIES(SYMBOL, COMPANY_NAME, COMPANY_DESC, SECTOR_ID) VALUES(?,?,?,?)",
-                security.getSymbol(), security.getCompanyName(), security.getDescription(), security.getSectorId());
-    }
-    
-    public void updateSecurity(Security security) {
-    	throw new UnsupportedOperationException();
-    }
-    
-    public void deleteSecurity(Security security) {
-    	throw new UnsupportedOperationException();
-    }
-    
-    public List<Security> getAllSecurities() {
-    	throw new UnsupportedOperationException();
-    }
+	public int saveSecurity(Security security) {
+		return jdbcTemplate.update(
+				"INSERT INTO SECURITIES(SYMBOL, COMPANY_NAME, COMPANY_DESC, SECTOR_ID) VALUES(?,?,?,?)",
+				security.getSymbol(), security.getCompanyName(), security.getDescription(), security.getSectorId());
+	}
 
-    public Security getSecurityBySymbol() {
-    	throw new UnsupportedOperationException();
-    }
-    
-    public List<Security> getSecuritiesBySector(Sector sector) {
-    	throw new UnsupportedOperationException();
-    }
-    
-    class SecurityRowMapper implements RowMapper<Security> {
+	public int updateSecurity(Security security) {
+		return jdbcTemplate.update("UPDATE SECURITIES SET SYMBOL = ?, COMPANY_NAME = ?, COMPANY_DESC = ? WHERE SECTOR_ID = ?",
+				security.getSymbol(), security.getCompanyName(), security.getDescription(), security.getSectorId());
+	}
+
+	public int deleteSecurity(Security security) {
+		return jdbcTemplate.update("DELETE SECURITIES WHERE SECTOR_ID = ?",
+				security.getSectorId());
+	}
+
+	public List<Security> getAllSecurities() {
+		return jdbcTemplate.query("SELECT * from SECURITIES", new SecurityRowMapper());
+	}
+
+	public Security getSecurityBySymbol(String symbol) {
+		return (Security)jdbcTemplate.query("SELECT * from SECURITIES where SYMBOL = ?",new Object[] {symbol} , new SecurityRowMapper());
+	}
+
+	public List<Security> getSecuritiesBySectorId(Sector sector) {
+		return jdbcTemplate.query(new PreparedStatementCreator() {
+
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement stmt = con.prepareStatement("SELECT * from SECURITIES where SECTOR_ID = ?");
+				stmt.setInt(1, sector.getId());
+				return stmt;
+			}
+		}, new SecurityRowMapper());
+	}
+
+	class SecurityRowMapper implements RowMapper<Security> {
 
 		@Override
 		public Security mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -58,7 +71,7 @@ public class SecurityRepository {
 			security.setDescription(rs.getString("COMPANY_DESC"));
 			security.setSectorId(rs.getInt("SECTOR_ID"));
 			return security;
-		}	
+		}
 	}
- 
+
 }
