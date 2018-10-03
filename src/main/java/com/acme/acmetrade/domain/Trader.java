517f46fe32bf42f850f0269972f5e206b45945e4
Antonyraj, Florence <Florence.antonyraj@fmr.com>
2018-10-03 14:46:19 -0400

    Domain objs

---
+++ src/main/java/com/acme/acmetrade/domain/Trader.java
@@ -0,0 +1,67 @@
+package com.acme.acmetrade.domain;
+
+public class Trader {
+	
+	private int traderId;
+	private String firstName;
+	private String lastName;	
+	private int phoneNum;
+	private String email ;
+	private String address;	
+	private String companyName;
+
+	public int getTraderId() {
+		return traderId;
+	}
+	public void setTraderId(int traderId) {
+		this.traderId = traderId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(int phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public Trader(int traderId, String firstName, String lastName, int phoneNum, String email, String address,
+			String companyName) {
+		super();
+		this.traderId = traderId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNum = phoneNum;
+		this.email = email;
+		this.address = address;
+		this.companyName = companyName;
+	}	
+
+}
