e35f4052caab85e5f4f2514744d443d37d70d32b
William Ovanna <william.ovanna@fmr.com>
2018-10-03 10:51:03 -0400

    New repository class for MarketSector, unit tests

---
+++ src/main/java/com/acme/acmetrade/repository/MarketSectorRepository.java
@@ -0,0 +1,8 @@
+package com.acme.acmetrade.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MarketSectorRepository {
+
+}
---
+++ src/test/java/com/acme/acmetrade/repository/MarketSectorTest.java
@@ -0,0 +1,27 @@
+package com.acme.acmetrade.repository;
+
+import org.junit.Test;
+
+public class MarketSectorTest {
+
+	
+	@Test
+	public void addNewMarketSector() {
+		
+	}
+	
+	@Test
+	public void updateExistingMarketSector() {
+		
+	}
+	
+	@Test
+	public void deleteMarketSector() {
+		
+	}
+	
+	@Test 
+	public void findMarketSectorByName() {
+		
+	}
+}
